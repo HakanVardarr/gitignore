02f9a1e380f04916bf83d7c98684be6cee253a38
Hakan Vardar <hakovardo123@gmail.com>
2024-01-24 20:31:25 +0300

    Clean command

--- build.bat
+++ build.bat
@@ -9,20 +9,33 @@ set Arg=%1%
 
 
 if /i not "%Arg%"=="Release" if /i not "%Arg%"=="Debug" (
-    if exist "build\Release\gitignore.exe" (
-        build\Release\gitignore.exe %Arg%
+    if "%Arg%"=="Clean" (
+        echo Cleaning build directory...
+        rmdir /s /q build
         exit /b 0
     ) else (
-        if exist "build\Debug\gitignore.exe" (
-            build\Debug\gitignore.exe %Arg%
+        if exist "build\Release\gitignore.exe" (
+            build\Release\gitignore.exe %Arg%
             exit /b 0
         ) else (
-            exit /b 1
+            if exist "build\Debug\gitignore.exe" (
+                build\Debug\gitignore.exe %Arg%
+                exit /b 0
+            ) else (
+                echo Error: The gitignore executable was not found in either Release or Debug folders.
+                echo Usage: build.bat [Configuration] or [Language] 'To run the file'
+                exit /b 1
+            )
         )
     )
 )
 
-mkdir build
-cmake -B build -S .
+if not exist "build" (
+       mkdir build
+) 
 
-MSBuild.exe ./build/gitignore.sln /p:Configuration=%Arg%
+cmake -B build -S .
+MSBuild.exe ./build/gitignore.sln /p:Configuration=%Arg% /verbosity:q
+echo ----------------------------------------------------------------
+echo -- Your code is succesfully builded on build\%Arg%\gitignore.exe
+echo ----------------------------------------------------------------
