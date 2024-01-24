521f7bc5c59d446261dc085c92a30d8d39639bc4
Hakan Vardar <hakovardo123@gmail.com>
2024-01-24 20:09:38 +0300

    Update on bat file

--- build.bat
+++ build.bat
@@ -1,20 +1,28 @@
 @echo off
 
 if "%1"=="" (
-    echo Usage: build.bat [Configuration]
+    echo Usage: build.bat [Configuration] or [Language] 'To run the file'
     exit /b 1
 )
 
-set Configuration=%1%
+set Arg=%1%
 
 
-if /i not "%Configuration%"=="Release" if /i not "%Configuration%"=="Debug" (
-    echo Invalid build configuration. Use only "Release" or "Debug".
-    exit /b 1
+if /i not "%Arg%"=="Release" if /i not "%Arg%"=="Debug" (
+    if exist "build\Release\gitignore.exe" (
+        build\Release\gitignore.exe %Arg%
+        exit /b 0
+    ) else (
+        if exist "build\Debug\gitignore.exe" (
+            build\Debug\gitignore.exe %Arg%
+            exit /b 0
+        ) else (
+            exit /b 1
+        )
+    )
 )
 
 mkdir build
 cmake -B build -S .
 
-
-MSBuild.exe ./build/gitignore.sln /p:Configuration=%Configuration%
+MSBuild.exe ./build/gitignore.sln /p:Configuration=%Arg%
